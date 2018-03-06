e2f3cc4cf8459aabc24bf5fedc1a10c4d5bb5361
Elaf Shah <ea.shah@outlook.com>
2018-03-06 08:34:52 +0300

    Add project files.

---
+++ Release2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Release2.Startup))]
+namespace Release2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
