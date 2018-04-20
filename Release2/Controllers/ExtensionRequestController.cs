18a6907085392666d72c3f44eed1491763bd62fa
MF <MF@ME.COM>
2018-04-20 10:56:08 +0300

    fixed details action in extension controller

--- Release2/Controllers/ExtensionRequestController.cs
+++ Release2/Controllers/ExtensionRequestController.cs
@@ -172,7 +172,9 @@ namespace Release2.Controllers
                 LMSubmitId = extension.LMSubmitId,
                 LMSubmits = extension.LMSubmits.FullName,
                 ExtRequestSubmissionDate = extension.ExtRequestSubmissionDate,
-                HRAudits = extension.HRAudits.FullName,
+
+                //HACK changed below
+                HRAudits = (extension.HRAudits == null)?string.Empty: extension.HRAudits.FullName,
                 ExtRequestAuditDate = extension.ExtRequestAuditDate,
                 ExtNumber = extension.ExtNumber,
                 ExtReason = extension.ExtReason,
